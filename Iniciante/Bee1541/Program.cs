681428c619dcb7ec1c1c1ee54e42c561afd8446b
Gideval Santos <santosgideval@gmail.com>
2026-06-28 21:37:41 -0300

    feat: Problem 1541

---
+++ Iniciante/Bee1541/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        List<int[]> houseArea = new List<int[]>();
+        int[] values = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        while (values[0] != 0)
+        {
+            houseArea.Add(values);
+            values = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        }
+
+        foreach(int[] area in houseArea)
+        {
+            Console.WriteLine((int)Math.Sqrt((area[0] * area[1] * 100) / area[2]));
+        }
+
+    }
+
+}
+
+/*
+Versão aceita pela plataforma devido a versão do compilador do C# 
+C# (mono 5.10.1.20)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        List<int[]> houseArea = new List<int[]>();
+
+        int[] values = Console.ReadLine()
+            .Split(' ')
+            .Select(x => int.Parse(x))
+            .ToArray();
+
+        while (values[0] != 0)
+        {
+            houseArea.Add(values);
+
+            values = Console.ReadLine()
+                .Split(' ')
+                .Select(x => int.Parse(x))
+                .ToArray();
+        }
+
+        foreach (int[] area in houseArea)
+        {
+            Console.WriteLine((int)Math.Sqrt((area[0] * area[1] * 100) / area[2]));
+        }
+    }
+}
+
+*/
